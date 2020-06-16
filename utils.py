5bba363de1caab8071ce3f2a14ebcdc7b096757c
naitzki <thiago.gsantos03@gmail.com>
2020-06-16 14:19:50 -0300

    Add functions to classifier

---
+++ utils.py
@@ -0,0 +1,32 @@
+
+import spacy
+import numpy as np
+
+nlp = spacy.load("pt_core_news_sm")
+
+def tokenizador(texto):
+    doc = nlp(texto)
+    tokens_validos = []
+
+    for token in doc:
+       
+        e_valido = not token.is_stop and token.is_alpha
+
+        if e_valido:
+            tokens_validos.append(token.text.lower())
+
+    return tokens_validos
+
+def combinacao_de_vetores_por_soma(palavras, modelo):
+
+    vetor_resultante = np.zeros((1, 300))
+
+    for pn in palavras:
+        try:
+            vetor_resultante += modelo.get_vector(pn)
+
+        except KeyError:
+            pass
+
+    return vetor_resultante
+
